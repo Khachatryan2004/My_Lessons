64e7a8f004e0abc4da537a9ea35c1b0beec210ab
Shavarsh <shavarshxachatryan2004@gmail.com>
2023-05-15 21:12:08 +0400

    OOP-Lesson2

---
+++ Lesson18.py
@@ -0,0 +1,28 @@
+# խնդիր 1
+'''
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+rectangle = Rectangle(3, 4)
+area = rectangle.area()
+perimeter = rectangle.perimeter()
+
+print("Area ", area)
+print("Perimeter ", perimeter)
+'''
+
+
+# խնդիր 2
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
